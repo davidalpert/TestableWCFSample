6d96cf3f3fd8e963d4206ac3386b94f442f57b08
David Alpert <david@spinthemoose.com>
2010-11-24 23:12:14 -0600

    add a test: try to create the service client

---
+++ Untestable/UntestableWCFSample.Tests/CatalogService_Facts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace UntestableWCFSample.Tests
+{
+    public class CatalogService_Facts
+    {
+        [Fact]
+        public void can_create()
+        {
+            CatalogServiceClient serviceClient = new CatalogServiceClient();
+        }
+    }
+}
